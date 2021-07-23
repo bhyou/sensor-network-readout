b3e049241d781c8ed9bb66e2f1a1ab55c402f760
bhyou <bhyou@foxmail.com>
2021-07-23 11:20:19 +0800

    delete package and config file

--- serial_agent/serial_config.sv
+++
@@ -1,8 +0,0 @@
-/*************************************************************************
- > Copyright (C) 2021 Sangfor Ltd. All rights reserved.
- > File Name   : serial_agent/serial_config.sv
- > Author      : bhyou
- > Mail        : bhyou@foxmail.com 
- > Created Time: Fri 16 Jul 2021 01:30:26 PM CST
- ************************************************************************/
- 
